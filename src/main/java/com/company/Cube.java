2cabddfdebebc8bb8ceafa7a86ac36abee641909
relfick <relfick@yandex.ru>
2020-02-12 13:46:56 +0300

    Добавил поворот любого слоя

--- src/main/java/com/company/Cube.java
+++ src/main/java/com/company/Cube.java
@@ -20,20 +20,24 @@ public class Cube {
         }
     }
 
-    // for my tests
+    // for tests
     void ChangeTheSquare(int side, int i, int j, int color) {
         this.sides[side].squares[i][j] = color;
     }
 
-    // direction: 0-Up, 1-Down, 2-Left, 3-Right
-    void turnTheCube(int direction) {
+    // direction: 0-Up, 1-Down, 2-Left, 3-Right, 4-clockwise, 5-counterclockwise
+    void TurnTheCube(int direction) {
         Side temp;
         switch (direction) {
             case 0:
                 temp = sides[0];
                 sides[0] = sides[5];
                 sides[5] = sides[2];
+                TurnTheFace(5, 0);
+                TurnTheFace(5, 0);
                 sides[2] = sides[4];
+                TurnTheFace(2, 0);
+                TurnTheFace(2, 0);
                 sides[4] = temp;
                 TurnTheFace(1, 0);
                 TurnTheFace(3, 1);
@@ -42,15 +46,18 @@ public class Cube {
                 temp = sides[0];
                 sides[0] = sides[4];
                 sides[4] = sides[2];
+                TurnTheFace(4, 0);
+                TurnTheFace(4, 0);
                 sides[2] = sides[5];
+                TurnTheFace(2, 0);
+                TurnTheFace(2, 0);
                 sides[5] = temp;
                 TurnTheFace(1, 1);
                 TurnTheFace(3, 0);
                 break;
             case 2:
                 temp = sides[0];
-                for (int i = 0; i < 3; i++)
-                    sides[i] = sides[(i + 1) % 4];
+                System.arraycopy(sides, 1, sides, 0, 3);
                 sides[3] = temp;
                 TurnTheFace(4, 0);
                 TurnTheFace(5, 1);
@@ -62,6 +69,23 @@ public class Cube {
                 TurnTheFace(4, 1);
                 TurnTheFace(5, 0);
                 break;
+            case 4:
+                temp = sides[1];
+                sides[1] = sides[4]; TurnTheFace(1, 0);
+                sides[4] = sides[3]; TurnTheFace(4, 0);
+                sides[3] = sides[5]; TurnTheFace(3, 0);
+                sides[5] = temp; TurnTheFace(5, 0);
+                TurnTheFace(0, 0);
+                TurnTheFace(2, 1);
+                break;
+            case 5:
+                temp = sides[1];
+                sides[1] = sides[5]; TurnTheFace(1, 1);
+                sides[5] = sides[3]; TurnTheFace(5, 1);
+                sides[3] = sides[4]; TurnTheFace(4, 1);
+                sides[4] = temp; TurnTheFace(4, 1);
+                TurnTheFace(0, 1);
+                TurnTheFace(2, 0);
         }
     }
 
@@ -90,11 +114,57 @@ public class Cube {
         }
     }
 
-    // axis: x, y, z - 0, 1, 2
-    // side: 0 - (size-1), count from left
-    // view against the axis, 0 - clockwise, 1 - counterclockwise
-    void TurnTheSide(int axis, int side, int direction) {
+    private void TurnFrontLayerToRight(int layer) {
+        int[] temp = new int[size];
+        for (int i = 0; i < size; i++)
+            temp[i] = sides[3].squares[layer][i]; // создали temp слоя грани 3
 
+        for (int i = 3; i > 0; i--)
+            for (int j = 0; j < size; j++)
+                sides[i].squares[layer][j] = sides[i - 1].squares[layer][j];
+
+        for (int i = 0; i < size; i++)
+            sides[0].squares[layer][i] = temp[i];
+
+        if (layer == 0) TurnTheFace(4, 1);
+        if (layer == size - 1) TurnTheFace(5, 0);
     }
 
+    private void TurnFrontLayerToLeft(int layer) {
+        int[] temp = new int[size];
+        for (int i = 0; i < size; i++)
+            temp[i] = sides[0].squares[layer][i]; // создали temp слоя грани 0
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < size; j++)
+                sides[i].squares[layer][j] = sides[i + 1].squares[layer][j];
+
+        for (int i = 0; i < size; i++)
+            sides[3].squares[layer][i] = temp[i];
+
+        if (layer == 0) TurnTheFace(4, 0);
+        if (layer == size - 1) TurnTheFace(5, 1);
+    }
+
+    // axis: y-0, x-1, z-2 (origin - back side, the bottom left square; y - front, to you; x - right; z - up)
+    // layer: 0 - (size-1), count from origin
+    // direction: 0 - clockwise, 1 - counterclockwise; (view against the axis)
+    void TurnTheSide(int axis, int layer, int direction) {
+        if (axis == 2) {
+            if (direction == 1) TurnFrontLayerToRight(size - layer - 1);
+            else TurnFrontLayerToLeft(size - layer - 1);
+        }
+        else if (axis == 1)  {
+                TurnTheCube(4); // Clockwise
+                if (direction == 0) TurnFrontLayerToRight(layer);
+                else TurnFrontLayerToLeft(layer);
+                TurnTheCube(5); // CounterClockwise
+        }
+        else if (axis == 0) {
+                TurnTheCube(1); // Down
+                if (direction == 0) TurnFrontLayerToRight(layer);
+                else TurnFrontLayerToLeft(layer);
+                TurnTheCube(0); // Up
+        }
+    }
 }
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -6,17 +6,17 @@ public class Main {
     public static void main(String[] args) {
         Cube cube = new Cube(3);
 
-        cube.ChangeTheSquare(4, 0, 0, 7);
-        cube.ChangeTheSquare(4, 1, 1, 7);
-        cube.ChangeTheSquare(5, 0, 0, 8);
-        cube.ChangeTheSquare(5, 1, 1, 8);
+//        cube.ChangeTheSquare(0, 0, 0, 7);
+//        cube.ChangeTheSquare(0, 1, 1, 7);
+//        cube.ChangeTheSquare(2, 0, 0, 8);
+//        cube.ChangeTheSquare(2, 1, 1, 8);
 
-        cube.turnTheCube(2);
-        System.out.println("Left");
-        cube.showTheStatus();
-
-        cube.turnTheCube(3);
-        System.out.println("Right");
+        for (int i = 0; i < 6; i++) {
+            cube.TurnTheSide(1, 2, 0);
+            cube.TurnTheSide(2, 2, 0);
+            cube.TurnTheSide(1, 2, 1);
+            cube.TurnTheSide(2, 2, 1);
+        }
         cube.showTheStatus();
     }
 }
